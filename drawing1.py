796b8b8ac0a535ba587fa46fdb5d9b5a6c9a72b3
nagomin25 <chikuwakonbu@gmail.com>
2025-03-22 19:52:23 +0900

    main.pyの作成

--- drawing1.py
+++
@@ -1,8 +0,0 @@
-import pyxel
-
-pyxel.init(160,120, title="Pyxel Drawing")
-
-pyxel.pset(10, 10, 7)
-pyxel.line(150, 10, 10, 110, 8)
-
-pyxel.show()
---
+++ main.py
@@ -0,0 +1,14 @@
+import pyxel
+
+
+class Game:
+    def __init__(self):
+
+        pyxel.init(128, 128, title="testGame")
+
+        pyxel.load("./asset.pyxres")
+
+
+
+
+Game()
